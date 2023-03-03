2c3a5110fb205e485ff0c4fc40b67b0c13004689
Apiwat Singharach <hohokz@hotmail.com>
2023-03-03 14:43:58 +0700

    first commit

---
+++ src/main/java/com/example/APILottory/ApiLottoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.APILottory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLottoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLottoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APILottory/ApiLottoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APILottory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLottoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
